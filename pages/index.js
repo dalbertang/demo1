b09e6c53c8c84055d0863f1ea145b8ec60afe87d
Dalbert Ang <da@architech.ca>
2023-06-22 14:31:53 -0400

    feat: text update

--- pages/index.js
+++ pages/index.js
@@ -19,11 +19,6 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
           <a href="https://nextjs.org/learn" className={styles.card}>
             <h3>Learn &rarr;</h3>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
